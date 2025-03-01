cefc44a3db407f91ffff289977139b5260206fdb
Lee Hyunsoo <philip21@snu.ac.kr>
2025-03-01 03:37:52 -0700

    edit

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd alg_v3_gcn_learnable_time_aws/
-sudo -H -u ubuntu bash -c "python3 train_ours.py --config ./cfgs/dataset_cfgs/lidarhuman26M_lite.yaml"
+cd alg_v5_diffusion_aws/
+sudo -H -u ubuntu bash -c "python3 train_ours.py --config ./cfgs/dataset_cfgs/soulart.yaml"
