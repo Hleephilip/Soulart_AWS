22561b69b72ca9fcf7c4c9a8fce0925d6ccc42d2
Lee Hyunsoo <philip21@snu.ac.kr>
2025-03-01 23:16:03 -0700

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd alg_v3_aws/
-sudo -H -u ubuntu bash -c "python3 train_ours.py --config ./cfgs/dataset_cfgs/soulart.yaml"
+cd movin_reproduction_aws/
+sudo -H -u ubuntu bash -c "python3 train_movin.py --config ./cfgs/soulart.yaml"
